df3fee0676be8826e613f0c038de1de4a419faaf
Roberto "Pena" Spinelli <penadoxo@gmail.com>
2024-10-12 22:31:51 -0300

    Update animotion.py

--- animotion/animotion.py
+++ animotion/animotion.py
@@ -2,7 +2,50 @@
 import time
 import threading
 from typing import List, Tuple, Callable
-from .interpolators import linear_interpolation, ease_in, ease_out, ease_in_out
+#from .interpolators import linear_interpolation, ease_in, ease_out, ease_in_out
+
+def linear_interpolation(start_value: float, end_value: float, t: float) -> float:
+    """
+    Interpolação linear entre dois valores.
+    :param start_value: Valor inicial.
+    :param end_value: Valor final.
+    :param t: Proporção entre 0 e 1.
+    :return: Valor interpolado.
+    """
+    return start_value + t * (end_value - start_value)
+
+def ease_in(start_value: float, end_value: float, t: float) -> float:
+    """
+    Interpolação ease-in, que começa lenta e acelera.
+    :param start_value: Valor inicial.
+    :param end_value: Valor final.
+    :param t: Proporção entre 0 e 1.
+    :return: Valor interpolado.
+    """
+    return start_value + (end_value - start_value) * (t ** 2)
+
+def ease_out(start_value: float, end_value: float, t: float) -> float:
+    """
+    Interpolação ease-out, que começa rápida e desacelera.
+    :param start_value: Valor inicial.
+    :param end_value: Valor final.
+    :param t: Proporção entre 0 e 1.
+    :return: Valor interpolado.
+    """
+    return start_value + (end_value - start_value) * (1 - (1 - t) ** 2)
+
+def ease_in_out(start_value: float, end_value: float, t: float) -> float:
+    """
+    Interpolação ease-in-out, que começa lenta, acelera e depois desacelera.
+    :param start_value: Valor inicial.
+    :param end_value: Valor final.
+    :param t: Proporção entre 0 e 1.
+    :return: Valor interpolado.
+    """
+    if t < 0.5:
+        return start_value + (end_value - start_value) * (2 * t) ** 2 / 2
+    else:
+        return start_value + (end_value - start_value) * (1 - (-2 * t + 2) ** 2 / 2)
 
 class Animotion:
     def __init__(self, duration: float, update_function: Callable[[float], None], interpolator: Callable = linear_interpolation):
