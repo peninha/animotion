b96b37fccfb98a906d4a1eec128c62542c01858d
Roberto "Pena" Spinelli <penadoxo@gmail.com>
2024-10-14 11:28:32 -0300

    update

--- tests/test_03.py
+++ tests/test_03.py
@@ -9,12 +9,12 @@ def update_function(mao, olho, sobran_dir, sobran_esq, boca, emocao):
     print(f"mao {mao}, olho {olho}, sobran_dir {sobran_dir}, sobran_esq {sobran_esq}, boca {boca}, emocao {emocao}")
 
 # Criar uma instância da classe Animotion com duração de 10 segundos
-animacao = Animotion(5, update_function, interpolator="ease_in_out", mode="visualize", delta_t=0.05)
+animacao = Animotion(10, update_function, interpolator="linear", mode="visualize", delta_t=0.05, value_names=["mao", "olho", "sobran_esq", "sobran_dir", "boca", "emocao"])
 
 # Adicionar keyframes para animar valores múltiplos, incluindo floats e strings
 animacao.add_keyframe(0, (0, 50, 30, 40, 0, "bravo"))  # Tempo 0s, valores: 0.0 e "bravo"
 animacao.add_keyframe(5, (100, 80, 0, 0, 50, "neutro"))  # Tempo 5s, valores: 100.0 e "neutro"
-#animacao.add_keyframe(10, (200, 100, 30, 0, 0, "feliz"))  # Tempo 10s, valores: 200.0 e "feliz"
+animacao.add_keyframe(10, (200, 100, 30, 0, 0, "feliz"))  # Tempo 10s, valores: 200.0 e "feliz"
 
 # Executar a animação
 animacao.run()
